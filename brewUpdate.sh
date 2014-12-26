012b884547b30056d7f46dc9889b704f574bfe70
srwareham <srwareham@hotmail.com>
2014-12-26 13:37:25 -0500

    More Verbose brewUpdate.sh

--- brewUpdate.sh
+++ brewUpdate.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 date
+echo "Updating Homebrew:"
 brew update
+echo "Upgrading Homebrew:"
 brew upgrade
+echo "Cleaning up Homebrew:"
+brew cleanup
+echo "Updating Brew Cask:"
+brew cask update
+echo "Cleaning Brew Cask:"
+brew cask cleanup
 echo "Update Complete:"
